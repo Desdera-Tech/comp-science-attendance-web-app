241fc86e62ccb7ae2b27ecd8d1db8f841ed7460c
Desdera <desderatech@gmail.com>
2026-02-08 12:28:59 +0100

    fix: add overflow-hidden class to SidebarInset in Admin and Student layouts

--- app/(app)/(dash)/admin/layout.tsx
+++ app/(app)/(dash)/admin/layout.tsx
@@ -16,7 +16,7 @@ export default async function AdminPagesLayout({
   return (
     <SidebarProvider>
       <AppSidebar isAdmin={true} />
-      <SidebarInset>
+      <SidebarInset className="overflow-hidden">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
